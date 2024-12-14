a53c63448479d45a99f434185be5db223b22b651
DafferNx <filbertdeven20@gmail.com>
2024-12-14 11:24:22 +0700

    prosedur tampilan daftar uang

--- MoneyChanger.java
+++ MoneyChanger.java
@@ -2,6 +2,11 @@
 import java.util.Scanner;
 
 public class MoneyChanger {
+    
+    static void daftarUang(double idr, double usd, double yen){
+        System.out.println("Rupiah\t\tUS Dollar\t\tYen");
+        System.out.printf("%.2f\t\t%.2f\t\t%.2f",idr,usd,yen);
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
