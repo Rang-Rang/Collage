9422ecd4142fcf6db92f62ae6b855c7ee7076465
Rangga Luthfiendi <echo.rangrang@gmail.com>
2024-12-14 10:56:48 +0700

    nambah spasi

--- MoneyChanger.java
+++ MoneyChanger.java
@@ -5,9 +5,10 @@ public class MoneyChanger {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int t = sc.nextInt();
         for (int i = 0; i < args.length; i++) {
-            
+
         }
     }
 }
